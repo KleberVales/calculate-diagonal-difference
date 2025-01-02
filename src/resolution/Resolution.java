82e4a57f9f818677ce5485406d7e7adfc35a9fb0
Kleber Vales <ksvales@outlook.pt>
2025-01-02 13:29:41 -0300

    Resolution problem

--- src/resolution/Resolution.java
+++ src/resolution/Resolution.java
@@ -1,10 +1,61 @@
 package resolution;
 
-public class Resolution {
+import static java.util.stream.Collectors.toList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Result {
+
+	public static int diagonalDifference(List<List<Integer>> arr) {
+
+		int primaryDiagonalSum = 0;
+		int secondaryDiagonalSum = 0;
+
+		int n = arr.size();
+
+		for (int i = 0; i < n; i++) {
+			primaryDiagonalSum += arr.get(i).get(i);
+			secondaryDiagonalSum += arr.get(i).get(n - i - 1);
+		}
+
+		return Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
 
 	}
 
+}
+
+public class Resolution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<List<Integer>> arr = new ArrayList<>();
+
+		IntStream.range(0, n).forEach(i -> {
+			try {
+				arr.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt)
+						.collect(toList()));
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		int result = Result.diagonalDifference(arr);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
